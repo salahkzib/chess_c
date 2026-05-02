ec0213c37ee2de88e445c9ee7a55895ee2587ab3
salah <kzibsalaheddine@gmail.com>
2026-05-02 16:42:09 +0100

    add logic of some function

--- AppProgram.c
+++ AppProgram.c
@@ -1,2 +1,86 @@
 #include <stdio.h>
+#include "AppUI.c"
 
+
+//array[][][] = {8*8*2}
+struct Player1
+{
+    int *Who;//point on an array of pieces of the player {piece with its *point *ability{true , false}}
+    int array[];
+};
+struct Player2
+{
+    int *Who;//point on an array of pieces of the player {piece with its *point *ability{true , false}}
+    int array[];
+};
+
+
+/*
+player1 move
+Check
+Moves(player 2)
+Check
+player move
+Check
+Moves PLayer1
+.....
+
+
+
+Move*/
+
+
+void PawnMovement(){
+//possible moves
+//switch board
+//int num = 1, -1 (color pawn s)
+/*
+if(!ability){
+return NULL
+}
+int index = pawn.index; //{x, y}
+side = pawn.Side;
+x, y;
+count = pawn.count;
+l[][] = {{{0, num},true{piece}}, {0, 2*num}//condition count == 0, {1, num}, {-1, num} // condition en passant}
+List pm[][] = {};
+int ArrayUsed[2];
+for(int i = 0; i < 4; i++){
+ArrayUsed = {l[i][0] + index[0]}
+if(ArrayUsed >= 0 and < 8 )
+
+}
+promoting
+jjjjjjjj
+
+
+
+
+
+iiiiiiii
+*/
+}
+void KingMovement(){
+
+}
+void QueenMovement(){
+
+}
+void RookMovement(){
+
+}
+void BishopMovement(){
+/*
+bp = Bishop.index;
+
+for(int j = 0; j < )
+
+*/
+}
+void NightMovement(){
+/*
+ability
+in board
+square : empty, piece diff color
+*/
+}
--- AppUI.c
+++ AppUI.c
@@ -1,7 +1,85 @@
 #include <stdio.h>
+#include "AppProgram.c"
 
 int main()
 {
+    //glade
+    /*
+    Variables_To_Create:
+    For Board(Squares are buttons):
+    Square A 1
+    Square A 2
+    Square A 3
+    Square A 4
+    Square A 5
+    Square A 6
+    Square A 7
+    Square A 8
+    Square B 1
+    Square B 2
+    Square B 3
+    Square B 4
+    Square B 5
+    Square B 6
+    Square B 7
+    Square B 8
+    Square C 1
+    Square C 2
+    Square C 3
+    Square C 4
+    Square C 5
+    Square C 6
+    Square C 7
+    Square C 8
+    Square D 1
+    Square D 2
+    Square D 3
+    Square D 4
+    Square D 5
+    Square D 6
+    Square D 7
+    Square D 8
+    Square E 1
+    Square E 2
+    Square E 3
+    Square E 4
+    Square E 5
+    Square E 6
+    Square E 7
+    Square E 8
+    Square F 1
+    Square F 2
+    Square F 3
+    Square F 4
+    Square F 5
+    Square F 6
+    Square F 7
+    Square F 8
+    Square G 1
+    Square G 2
+    Square G 3
+    Square G 4
+    Square G 5
+    Square G 6
+    Square G 7
+    Square G 8
+    Square H 1
+    Square H 2
+    Square H 3
+    Square H 4
+    Square H 5
+    Square H 6
+    Square H 7
+    Square H 8
 
-    return 0;
+    7
+    ^
+    |
+    0 -> 7
+    For Pieces(are also buttons):
+    
+    */
+   
+
+   return 0;
 }
